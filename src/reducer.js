13e901ec61ffa9bb78badfb7fb3fb0f2ba0d35c9
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2022-10-04 22:52:44 +0100

    3 files modified

--- src/reducer.js
+++ src/reducer.js
@@ -5,10 +5,19 @@ const reducer = (state, action) => {
   if (action.type === 'REMOVE') {
     return {
       ...state,
-      cart: state.cart.filter((cartItem) => cartItem.id !==  action.payload),
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
     };
   }
-  return state;
+  if (action.type === 'INCREASE') {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
+  }
+  if (action.type) return state;
 };
 
 export default reducer;

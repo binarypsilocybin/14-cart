6671042a872d3900484c0a1315f88aeedd89001e
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2022-10-06 15:53:27 +0100

    1 file modified

--- src/reducer.js
+++ src/reducer.js
@@ -29,11 +29,11 @@ const reducer = (state, action) => {
     return { ...state, cart: tempCart };
   }
   if (action.type === 'GET_TOTALS') {
-    const { total, amount } = state.cart.reduce(
+    let { total, amount } = state.cart.reduce(
       (cartTotal, cartItem) => {
         const { price, amount } = cartItem;
         const itemTotal = price * amount;
-        cartTotal.total += itemTotal
+        cartTotal.total += itemTotal;
         cartTotal.amount += amount;
         return cartTotal;
       },
@@ -42,6 +42,7 @@ const reducer = (state, action) => {
         amount: 0,
       }
     );
+    total = parseFloat(total.toFixed(2));
 
     return { ...state, total, amount };
   }
